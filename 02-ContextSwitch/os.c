ce15c92057619fe8a5b417386123d410c45754aa
liang <liang257@gmail.com>
2025-07-25 01:10:16 +0800

    [dev] 1.add 02-contextswitch folder

---
+++ 02-ContextSwitch/os.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+
+#define REG8(reg) *((uint8_t*) reg)
+
+#define UART_BA 0x10000000
+#define THR (UART_BA + 0x0)
+#define LSR (UART_BA + 0x5)
+#define TE_MSK (1<<6)
+
+void __puts(char* s){
+		while(( REG8(LSR) & TE_MSK) == 0 );
+		REG8(THR) = *s;
+	
+}
+
+void lib_puts(char* s){
+	while( *s != '\0'){
+		__puts(s++);
+	}
+}
+
+int os_main(void){
+		lib_puts("HelloOS\n");
+		while(1);
+		return 0;
+}
